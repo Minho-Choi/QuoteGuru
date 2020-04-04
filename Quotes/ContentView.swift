925425b82d176fd4caba266233f6f88ae1afeafe
Minho Choi <2153373@naver.com>
2020-04-04 16:01:54 +0900

    Initial Commit

---
+++ Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quotes
+//
+//  Created by Minho Choi on 2020/04/04.
+//  Copyright © 2020 Minho Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
